ad4298addffbac5b87d62e977b8a904ad1055fb1
আপনার নাম <rajim.uddin4sa@gmail.com>
2026-07-22 12:11:07 +0600

    page fix

---
+++ page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px", fontFamily: "sans-serif" }}>
+      <h1>Care Clear Saas is Live!</h1>
+    </div>
+  );
+}
